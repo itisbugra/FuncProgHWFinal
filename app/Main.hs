d67a846525070342b89d621570ea07cfe3d60a19
Buğra Ekuklu <ekuklu@icloud.com>
2018-05-22 17:50:24 +0300

    Agent assert argument list length.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@ module Main where
 import qualified CLI as CLI
 import qualified CharTrie as CharTrie
 
+import System.IO
+import System.Environment
+
 main :: IO ()
 main = do
   agent
@@ -10,10 +13,21 @@ main = do
 agent :: IO ()
 agent = do
   CLI.printManifest
-  loop CharTrie.empty
-  return ()
+  args <- getArgs
+  case length args of
+    1 -> do
+      words <- readWords $ head args
+      loop $ CharTrie.insertList words
+      return ()
+    _ -> do
+      return ()
 
 loop :: CharTrie.Trie -> IO CharTrie.Trie
 loop trie = do
   nextTrie <- CLI.promptAction trie
   loop nextTrie
+
+readWords :: String -> IO [String]
+readWords fileName = do
+  contents <- readFile fileName
+  return $ lines contents
