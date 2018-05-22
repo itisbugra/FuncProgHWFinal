0dfb09a58268b04f7771a5c1f6e590397c1e7c91
Buğra Ekuklu <ekuklu@icloud.com>
2018-05-22 03:00:09 +0300

    Application instantiation implementation.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import qualified CLI as CLI
+import qualified CharTrie as CharTrie
 
 main :: IO ()
-main = someFunc
+main = do
+  agent
+
+agent :: IO ()
+agent = do
+  CLI.printManifest
+  loop CharTrie.empty
+  return ()
+
+loop :: CharTrie.Trie -> IO CharTrie.Trie
+loop trie = do
+  nextTrie <- CLI.promptAction trie
+  loop nextTrie
